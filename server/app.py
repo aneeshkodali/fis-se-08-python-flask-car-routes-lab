275e8153a552eafcdcf4f2513543babe7ce6816f
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-03-05 08:39:30 +0100

    Add initial routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,15 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome to Flatiron Cars"
+
+@app.route('/<string:model>')
+def model_exists(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    return f"No models called {model} exists in our catalog"
